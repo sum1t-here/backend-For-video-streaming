486d54489417e0b2db82a59e82048cf4c6e54791
sumit <mazumdarsumit3@gmail.com>
2024-01-03 11:16:39 +0530

    file upload setup done

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import { response } from "express";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+
+    // upload file on cloudinary
+    cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file uploaded succesfully
+    console.log("File is uploaded on cloudinary", response.url);
+    return response;
+  } catch (err) {
+    fs.unlinkSync(localFilePath); // remove the locally saved temp file if the upload operation is failed
+  }
+};
